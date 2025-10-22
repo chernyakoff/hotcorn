e4aff5cb83765e1909954b867fb531fdccd3dd2c
Michael Chernyakoff <chernyakoff@gmail.com>
2025-10-22 16:13:20 +0300

    added flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Hotcorn - Hot Corners and Edge Triggers for Hyprland";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # Build Rust package
+        packages.hotcorn = pkgs.rustPlatform.buildRustPackage {
+          pname = "hotcorn";
+          version = "0.1.0";
+          src = ./.;
+          buildInputs = [
+            pkgs.pkg-config
+            pkgs.libxcb
+            pkgs.alsaLib
+          ];
+        };
+
+        # Dev shell for development
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.pkg-config
+            pkgs.libxcb
+            pkgs.alsaLib
+          ];
+          shellHook = ''
+            echo "Hotcorn dev shell ready!"
+          '';
+        };
+      }
+    );
+}
