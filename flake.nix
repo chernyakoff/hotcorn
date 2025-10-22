43c3528e4f9627f39570ee27c3a4de5d3adaaf6f
Michael Chernyakoff <chernyakoff@gmail.com>
2025-10-22 17:19:53 +0300

    fix flake typo

--- flake.nix
+++ flake.nix
@@ -8,35 +8,22 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      let pkgs = import nixpkgs { inherit system; };
+      in {
         # Build Rust package
         packages.hotcorn = pkgs.rustPlatform.buildRustPackage {
           pname = "hotcorn";
           version = "0.1.0";
           src = ./.;
-          buildInputs = [
-            pkgs.pkg-config
-            pkgs.libxcb
-            pkgs.alsaLib
-          ];
+          buildInputs = [ pkgs.pkg-config pkgs.libxcb ];
         };
 
         # Dev shell for development
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.pkg-config
-            pkgs.libxcb
-            pkgs.alsaLib
-          ];
+          buildInputs = [ pkgs.rustc pkgs.cargo pkgs.pkg-config pkgs.libxcb ];
           shellHook = ''
             echo "Hotcorn dev shell ready!"
           '';
         };
-      }
-    );
+      });
 }
